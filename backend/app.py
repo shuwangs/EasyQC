753e5097664525e8386224a42e811ec5d560f247
shuwang425 <60482034+shuwang425@users.noreply.github.com>
2025-07-30 14:02:59 -0400

    add mock data

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Configure the dir for the later use
 
-UPLOAD_FOLDER = 'backend/uploads'
+UPLOAD_FOLDER = '/uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok = True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
