860c4df87c478d61ca45c8c7c8c099b70a1a96cd
shuwang425 <60482034+shuwang425@users.noreply.github.com>
2025-07-30 13:42:57 -0400

    Implement backend upload route

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 ### Step 1: initialize backend flask 
 - /: test if Flask can successfully run 
 - /upload: provide csv file and normalization paramters in 
+
+### Step 2: Achieve the /upload Routes logic 
+- receive the CSV file
+- read normalization parameters
+- save the file to uploads/
+- return a simple Json response. 
+  
+  
--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import os
+import os, uuid
 
 app = Flask(__name__)
 
@@ -9,6 +9,8 @@ UPLOAD_FOLDER = 'backend/uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok = True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+ALLOWED_NORMALIZATIONS = ['tic', 'is', 'qc', 'pqn', 'is+qc', 'is+pqn']
+
 @app.route('/')
 def index():
     return "Welcome to EasyQC! Please upload your CSV file for data quality checks."
@@ -16,7 +18,29 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # TODO:
+    if 'file' not in request.files:
+        return jsonify({'Error': 'No file provided'}), 400
+    
     file = request.files['file']
+
+    normalization = request.form.get('normalization', "none").lower()
+    if normalization not in ALLOWED_NORMALIZATIONS:
+        return jsonify({'Error': f'Invalid normalization method: {normalization}'}), 400
+
+    if file and file.filename.lower().endswith('.txt'):
+        filename = f"{uuid.uuid4().hex}.txt"
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(save_path)
+
+        return jsonify({
+            "message": "File uploaded successfully",
+            "filename": filename,
+            "normalization": normalization
+        }), 200
+
+    return jsonify({'error': 'Only CSV files are allowed'}), 400
+ 
+
 
 
 if __name__ == '__main__':
