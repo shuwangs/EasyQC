3b95b4493ebec74be2cbdab505d8f5b9473f3efa
Shu <shuw425@gmail.com>
2025-07-30 01:45:37 -0400

    get the app start

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # EasyQC 
 
-# The goal of this web app is to check the data quality of the metabolomics and lipidomics data
+##  The goal of this web app is to check the data quality of the metabolomics and lipidomics data
 
 
 
 
-# Step 1: initialize backend flask 
+### Step 1: initialize backend flask 
 - /: test if Flask can successfully run 
 - /upload: provide csv file and normalization paramters in 
--- backend/app.py
+++ backend/app.py
@@ -9,3 +9,15 @@ UPLOAD_FOLDER = 'backend/uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok = True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route('/')
+def index():
+    return "Welcome to EasyQC! Please upload your CSV file for data quality checks."
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # TODO:
+    file = request.files['file']
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
